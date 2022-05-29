272b3d1972ec163802eab22f46b03675e928c213
VlTok <vladimir.tokmeninov@mail.ru>
2022-05-29 19:39:29 +0300

    Добавление миграции

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,42 +1,40 @@
 create sequence hibernate_sequence start 1 increment 1;
 
-CREATE TABLE k_schedule
-(
-    s_id          int8         NOT NULL,
+create table k_couple (
+    c_id BIGINT not null,
+    c_audience varchar(255),
+    c_teacher varchar(255),
+    c_time timestamp not null,
+    c_title varchar(255) not null,
+    c_type varchar(255),
+    c_schedule_id BIGINT,
+    primary key (c_id)
+);
+
+create table k_schedule (
+    s_id BIGINT not null,
     s_day_of_week varchar(255) not null,
-    s_parity      varchar(255) not null,
-    s_team        varchar(255) not null,
-    s_author_id   int8,
+    s_filename_errors varchar(255),
+    s_filename_excel varchar(255),
+    s_parity varchar(255) not null,
+    s_team varchar(255) not null,
+    s_author_id BIGINT,
     primary key (s_id)
 );
 
-CREATE TABLE k_couple
-(
-    c_id          int8         NOT NULL,
-    c_audience    varchar(255) not null,
-    c_title       varchar(255) not null,
-    c_type        varchar(255) not null,
-    c_time        timestamp    not null,
-    c_schedule_id int8,
-    c_teacher_id  int8,
-    PRIMARY KEY (c_id)
-);
-
-create table user_roles
-(
-    user_u_id int8 not null,
-    roles     varchar(255)
-);
-
-CREATE TABLE k_user
-(
-    u_id              int8         NOT NULL,
+create table k_user (
+    u_id BIGINT not null,
     u_activation_code varchar(255),
-    u_active          boolean      not null,
-    u_email           varchar(255),
-    u_password        varchar(255) not null,
-    u_username        varchar(255) not null,
-    PRIMARY KEY (u_id)
+    u_active boolean,
+    u_email varchar(255) not null,
+    u_password varchar(255) not null,
+    u_username varchar(30) not null,
+    primary key (u_id)
+);
+
+create table user_roles (
+    user_u_id BIGINT not null,
+    roles varchar(255)
 );
 
 alter table if exists k_schedule
@@ -44,12 +42,8 @@ alter table if exists k_schedule
         foreign key (s_author_id) references k_user;
 
 alter table if exists k_couple
-    add constraint couple_user_fk
-        foreign key (c_teacher_id) references k_user;
-
-alter table if exists k_couple
     add constraint couple_schedule_fk
-        foreign key (c_schedule_id) references k_user;
+        foreign key (c_schedule_id) references k_schedule;
 
 alter table if exists user_roles
     add constraint user_roles_user_fk
